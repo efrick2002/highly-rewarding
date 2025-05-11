25aa1f2dfb44e7c6c9b7bd76d48c21d86384667f
Evan Frick <evanfrick@Evans-MacBook-Pro-2.local>
2025-05-11 12:07:53 -0700

    a

--- modeling.py
+++ modeling.py
@@ -596,6 +596,108 @@ def get_thurstone_mlp_reward_model_class(
     return RewardModel
 
 
+@register_model_class("thurstone-mlp-detached-var-reward-model")
+def get_thurstone_mlp_detached_var_reward_model_class(
+    model_type: str, tokenizer: PreTrainedTokenizer, init_type: str = "reset_params"
+) -> PreTrainedModel:
+    # Should construct and return the model class such that the trainer can call .from_pretrained on it.
+
+    transformer_model_cls, pretrained_model_cls = MODEL_TYPE_REGISTRY[model_type]
+
+    init_func = REGISTERED_INITS[init_type]
+
+    cls_token = tokenizer.cls_token_id
+
+    class RewardPretrainedModel(pretrained_model_cls):
+
+        def _init_weights(self, module):
+            std = self.config.initializer_range
+            if isinstance(module, nn.Linear):
+                init_func(module)  # was reset params
+                if module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, nn.Embedding):
+                module.weight.data.normal_(mean=0.0, std=std)
+                if module.padding_idx is not None:
+                    module.weight.data[module.padding_idx].zero_()
+
+    class RewardModel(RewardPretrainedModel):
+
+        def __init__(
+            self,
+            config,
+            **kwargs,
+        ):
+            super().__init__(config)
+
+            self.model = transformer_model_cls(config)
+
+            self.mean_head = nn.Sequential(
+                nn.Linear(
+                    in_features=config.hidden_size,
+                    out_features=config.hidden_size*4,
+                ),
+                nn.SiLU(),
+                nn.Linear(
+                    in_features=config.hidden_size*4,
+                    out_features=1,
+                ),
+            )
+
+            self.logvar_head = nn.Sequential(
+                nn.Linear(
+                    in_features=config.hidden_size,
+                    out_features=config.hidden_size*4,
+                ),
+                nn.SiLU(),
+                nn.Linear(
+                    in_features=config.hidden_size*4,
+                    out_features=1,
+                ),
+            )
+
+            self.post_init()
+
+        def get_input_embeddings(self):
+            return self.model.embed_tokens
+
+        def set_input_embeddings(self, value):
+            self.model.embed_tokens = value
+
+        def forward(self, input_ids, attention_mask):
+
+            hidden_outputs = self.model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                output_hidden_states=False,
+            ).last_hidden_state  # (bs, num_token, embed_dim)
+
+            cls_mask = input_ids == cls_token
+
+            cls_hidden_dim = hidden_outputs[cls_mask]
+
+            means = self.mean_head(cls_hidden_dim)
+            logvars = self.logvar_head(cls_hidden_dim.detach())
+
+            # The pairwise rewards are flattened, so we need to unflatten them. For now, we will assume it is always pairwise.
+            means = means.view(-1, 2)
+            logvars = logvars.view(-1, 2)
+
+            assert (
+                means.shape[0] * 2 == input_ids.shape[0] and means.shape[1] == 2
+            ), f"Means shape: {means.shape}, input_ids shape: {input_ids.shape}"
+            assert (
+                logvars.shape == means.shape
+            ), f"Logvars shape: {logvars.shape}, means shape: {means.shape}"
+
+            return ThurstoneRewardOutputs(
+                means=means,
+                logvars=logvars,
+            )
+
+    return RewardModel
+
+
 
 @register_model_class("thurstone-mlp-2-reward-model")
 def get_thurstone_mlp_2_reward_model_class(
@@ -744,6 +846,214 @@ def get_double_cls_thurstone_reward_model_class(
             self.logvar_head = nn.Linear(
                 in_features=config.hidden_size,
                 out_features=1,
+            )
+
+            self.post_init()
+
+        def get_input_embeddings(self):
+            return self.model.embed_tokens
+
+        def set_input_embeddings(self, value):
+            self.model.embed_tokens = value
+
+        def forward(self, input_ids, attention_mask):
+
+            hidden_outputs = self.model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                output_hidden_states=False,
+            ).last_hidden_state  # (bs, num_token, embed_dim)
+
+            cls_mean_mask = input_ids == cls_mean_token
+            cls_logvar_mask = input_ids == cls_logvar_token
+
+            cls_mean_hidden_dim = hidden_outputs[cls_mean_mask]
+            cls_logvar_hidden_dim = hidden_outputs[cls_logvar_mask]
+
+            means = self.mean_head(cls_mean_hidden_dim)
+            logvars = self.logvar_head(cls_logvar_hidden_dim)
+
+            # The pairwise rewards are flattened, so we need to unflatten them. For now, we will assume it is always pairwise.
+            means = means.view(-1, 2)
+            logvars = logvars.view(-1, 2)
+
+            assert (
+                means.shape[0] * 2 == input_ids.shape[0] and means.shape[1] == 2
+            ), f"Means shape: {means.shape}, input_ids shape: {input_ids.shape}"
+            assert (
+                logvars.shape == means.shape
+            ), f"Logvars shape: {logvars.shape}, means shape: {means.shape}"
+
+            return ThurstoneRewardOutputs(
+                means=means,
+                logvars=logvars,
+            )
+
+    return RewardModel
+
+@register_model_class("double-cls-mlp-thurstone-reward-model")
+def get_double_cls_mlp_thurstone_reward_model_class(
+    model_type: str, tokenizer: PreTrainedTokenizer, init_type: str = "reset_params"
+) -> PreTrainedModel:
+    # Should construct and return the model class such that the trainer can call .from_pretrained on it.
+
+    transformer_model_cls, pretrained_model_cls = MODEL_TYPE_REGISTRY[model_type]
+
+    init_func = REGISTERED_INITS[init_type]
+
+    cls_mean_token = tokenizer.cls_mean_id
+    cls_logvar_token = tokenizer.cls_logvar_id
+
+    class RewardPretrainedModel(pretrained_model_cls):
+
+        def _init_weights(self, module):
+            std = self.config.initializer_range
+            if isinstance(module, nn.Linear):
+                init_func(module)  # was reset params
+                if module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, nn.Embedding):
+                module.weight.data.normal_(mean=0.0, std=std)
+                if module.padding_idx is not None:
+                    module.weight.data[module.padding_idx].zero_()
+
+    class RewardModel(RewardPretrainedModel):
+
+        def __init__(
+            self,
+            config,
+            **kwargs,
+        ):
+            super().__init__(config)
+
+            self.model = transformer_model_cls(config)
+
+            self.mean_head = nn.Sequential(
+                nn.Linear(
+                    in_features=config.hidden_size,
+                    out_features=config.hidden_size*4,
+                ),
+                nn.SiLU(),
+                nn.Linear(
+                    in_features=config.hidden_size*4,
+                    out_features=1,
+                ),
+            )
+
+            self.logvar_head = nn.Sequential(
+                nn.Linear(
+                    in_features=config.hidden_size,
+                    out_features=config.hidden_size*4,
+                ),
+                nn.SiLU(),
+                nn.Linear(
+                    in_features=config.hidden_size*4,
+                    out_features=1,
+                ),
+            )
+
+            self.post_init()
+
+        def get_input_embeddings(self):
+            return self.model.embed_tokens
+
+        def set_input_embeddings(self, value):
+            self.model.embed_tokens = value
+
+        def forward(self, input_ids, attention_mask):
+
+            hidden_outputs = self.model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                output_hidden_states=False,
+            ).last_hidden_state  # (bs, num_token, embed_dim)
+
+            cls_mean_mask = input_ids == cls_mean_token
+            cls_logvar_mask = input_ids == cls_logvar_token
+
+            cls_mean_hidden_dim = hidden_outputs[cls_mean_mask]
+            cls_logvar_hidden_dim = hidden_outputs[cls_logvar_mask]
+
+            means = self.mean_head(cls_mean_hidden_dim)
+            logvars = self.logvar_head(cls_logvar_hidden_dim)
+
+            # The pairwise rewards are flattened, so we need to unflatten them. For now, we will assume it is always pairwise.
+            means = means.view(-1, 2)
+            logvars = logvars.view(-1, 2)
+
+            assert (
+                means.shape[0] * 2 == input_ids.shape[0] and means.shape[1] == 2
+            ), f"Means shape: {means.shape}, input_ids shape: {input_ids.shape}"
+            assert (
+                logvars.shape == means.shape
+            ), f"Logvars shape: {logvars.shape}, means shape: {means.shape}"
+
+            return ThurstoneRewardOutputs(
+                means=means,
+                logvars=logvars,
+            )
+
+    return RewardModel
+
+@register_model_class("double-cls-mlp-2-thurstone-reward-model")
+def get_double_cls_mlp_2_thurstone_reward_model_class(
+    model_type: str, tokenizer: PreTrainedTokenizer, init_type: str = "reset_params"
+) -> PreTrainedModel:
+    # Should construct and return the model class such that the trainer can call .from_pretrained on it.
+
+    transformer_model_cls, pretrained_model_cls = MODEL_TYPE_REGISTRY[model_type]
+
+    init_func = REGISTERED_INITS[init_type]
+
+    cls_mean_token = tokenizer.cls_mean_id
+    cls_logvar_token = tokenizer.cls_logvar_id
+
+    class RewardPretrainedModel(pretrained_model_cls):
+
+        def _init_weights(self, module):
+            std = self.config.initializer_range
+            if isinstance(module, nn.Linear):
+                init_func(module)  # was reset params
+                if module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, nn.Embedding):
+                module.weight.data.normal_(mean=0.0, std=std)
+                if module.padding_idx is not None:
+                    module.weight.data[module.padding_idx].zero_()
+
+    class RewardModel(RewardPretrainedModel):
+
+        def __init__(
+            self,
+            config,
+            **kwargs,
+        ):
+            super().__init__(config)
+
+            self.model = transformer_model_cls(config)
+
+            self.mean_head = nn.Sequential(
+                nn.Linear(
+                    in_features=config.hidden_size,
+                    out_features=config.hidden_size*2,
+                ),
+                nn.SiLU(),
+                nn.Linear(
+                    in_features=config.hidden_size*2,
+                    out_features=1,
+                ),
+            )
+
+            self.logvar_head = nn.Sequential(
+                nn.Linear(
+                    in_features=config.hidden_size,
+                    out_features=config.hidden_size*2,
+                ),
+                nn.SiLU(),
+                nn.Linear(
+                    in_features=config.hidden_size*2,
+                    out_features=1,
+                ),
             )
 
             self.post_init()
