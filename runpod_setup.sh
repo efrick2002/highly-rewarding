e07e4e52198921c3af1a83f1fa6b9090d99f928f
Evan Frick <evanfrick@Evans-MacBook-Pro-2.local>
2025-05-12 16:12:04 -0700

    a

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -10,7 +10,7 @@ uv venv .venv --python 3.10
 
 source .venv/bin/activate
 
-uv pip install wheel packaging
+uv pip install wheel packaging setuptools
 
 uv pip install -r requirements.txt
 uv pip install flash-attn==2.5.9.post1 --no-build-isolation
